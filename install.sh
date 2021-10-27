decfcf8875cc13adfa0b01f07535ca35cff9ffb0
Noah Broyles <noah@javamate.net>
2021-10-27 11:06:49 -0400

    Wow I need to go back to the basics

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "installing the 'robo' and 'generateSalt' commands..."
 sudo cp src/Robocrypt.py /usr/local/bin/robo
 sudo cp src/generateSalt.py /usr/local/bin/generateSalt
-sudo chmod 744 /usr/local/bin/robo
-sudo chmod 744 /usr/local/bin/generateSalt
+sudo chmod 751 /usr/local/bin/robo
+sudo chmod 751 /usr/local/bin/generateSalt
 echo "installation completed successfully."
