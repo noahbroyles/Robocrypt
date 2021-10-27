e8de4af6b2fac0ba9c5cb9d773429cd9d1eff5f8
Noah Broyles <noah@javamate.net>
2021-10-27 11:02:47 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 echo "installing the 'robo' and 'generateSalt' commands..."
 sudo cp src/Robocrypt.py /usr/local/bin/robo
 sudo cp src/generateSalt.py /usr/local/bin/generateSalt
-echo "Done!"
-echo
+echo "installation completed successfully."
