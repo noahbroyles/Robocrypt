afcb90b5afffecd4edbc17a5e7b3dda4b539a7d4
Noah Broyles <noah@javamate.net>
2021-10-27 11:01:46 -0400

    Added install script to copy commands to bin

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo "installing the 'robo' and 'generateSalt' commands..."
+sudo cp src/Robocrypt.py /usr/local/bin/robo
+sudo cp src/generateSalt.py /usr/local/bin/generateSalt
+echo "Done!"
+echo
